09e35d92b8942be86e18e6029c71cbb1c603aae8
Mateusz Chmielewski <hello@itsmattch.com>
2024-08-22 11:15:38 +0200

    Change base

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "Gildsmith",
   description: "Dev Docs",
+  base: "/devdocs/",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
